cbb184a17307a056c611094df1366608822da302
ly32222848 <ly32222848@gmail.com>
2025-01-25 11:33:02 +0800

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
   return (
     <html lang={locale}>
-      <body className='relative mx-auto flex min-h-screen flex-col bg-tap4-black text-white'>
+      <body className="relative mx-auto flex min-h-screen flex-col bg-background text-foreground">
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Toaster
             position='top-center'
